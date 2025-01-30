57cf9015a1f741fdbb34b0bf98c51aa6a33a77f3
Arvin Bohlool <abohloolk@gmail.com>
2025-01-30 18:11:35 +0330

    complete

--- linked-list/second.c
+++ linked-list/second.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// node structure
+typedef struct node
+{
+    int data;
+    struct node *next;
+} node;
+
+
 int main(int argc, char *argv[])
 {
+    node *list = NULL;
+
+    for (int i = 1;i < argc;i++)
+    {
+        node *n = malloc(sizeof(node));
+        int number = atoi(argv[i]);
+
+        if (n == NULL) {return 1;}
+
+        n->data = number;
+        n->next = list;
+        list = n;
+    }
+
+    // Printing the whole list
+    node *ptr = list;
+    while (ptr != NULL)
+    {
+        printf("%i\n", ptr->data);
+        ptr = ptr->next;
+    }
+
     return 0;
 }
