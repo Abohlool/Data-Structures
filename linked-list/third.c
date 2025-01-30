78acd65dc436c6a67ec00f282d1fbe7a15b77a9f
Arvin Bohlool <abohloolk@gmail.com>
2025-01-30 18:29:14 +0330

    prototypes
    function prototypes were added

--- linked-list/third.c
+++ linked-list/third.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Node Structure
 typedef struct node
 {
     int data;
     struct node *next;
 } node;
 
+node *newNode(int data);
+void printList(node *first);
 
 int main(int argc, char *argv[])
 {
-    
+    node *list = NULL;
     return 0;
 }
