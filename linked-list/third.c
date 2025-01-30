d3bcfc1bb95ad27811cfb94ac62a2606c7a5efbb
Arvin Bohlool <abohloolk@gmail.com>
2025-01-30 18:54:03 +0330

    main completed

--- linked-list/third.c
+++ linked-list/third.c
@@ -14,5 +14,14 @@ void printList(node *first);
 int main(int argc, char *argv[])
 {
     node *list = NULL;
+    for (int i = 1;i < argc;i++)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL) {return 1;}
+
+        n = newNode(atoi(argv[i]));
+    }
+    printList(list);
+
     return 0;
 }
