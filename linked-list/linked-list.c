14fb5eb5f7a2d5bcb1ab05b546123e9af4c384f3
Arvin Bohlool <abohloolk@gmail.com>
2025-01-30 10:36:31 +0330

    Node Data Structure added

--- linked-list/linked-list.c
+++ linked-list/linked-list.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Node Data Structure
+typedef struct node
+{
+    int data;
+    node *next;
+} node;
+
 int main(int argc, char *argv[])
 {
-    
+
     return 0;
 }
