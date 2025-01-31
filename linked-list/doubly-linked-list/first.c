904ab97b96881db036dbeb309df633258523d749
Arvin Bohlool <abohloolk@gmail.com>
2025-01-31 10:05:18 +0330

    node data structure

--- linked-list/doubly-linked-list/first.c
+++ linked-list/doubly-linked-list/first.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// node data structure
+typedef struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+} node;
+
 int main(void)
 {
     
