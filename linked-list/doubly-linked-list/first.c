5251a4e555ad64c7c3715a0ea748353d5006bf80
Arvin Bohlool <abohloolk@gmail.com>
2025-02-04 09:18:53 +0330

    loop

--- linked-list/doubly-linked-list/first.c
+++ linked-list/doubly-linked-list/first.c
@@ -11,6 +11,17 @@ typedef struct node
 
 int main(void)
 {
+    node *list = NULL;
+    list->prev = NULL;
+
+    for (int i = 0;i < 5;i++)
+    {
+        node *n = list;
+        if (n == NULL) {return 1;}
+        scanf("%i", n->data);
+        n->prev = list;
+        n->next = NULL;
+    }
     
     return 0;
 }
